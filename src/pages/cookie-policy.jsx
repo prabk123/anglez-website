802cf573f9462f500cac5c67a434efb57f4fd59d
prabk123 <prabodhkakodkar@gmail.com>
2023-02-12 16:52:48 +0000

    add cookie and privacy policy

---
+++ src/pages/cookie-policy.jsx
@@ -0,0 +1,5 @@
+import LegalPage from "../components/LegalPage";
+
+export default function CookiePolicy() {
+  return <LegalPage title="Cookie Policy" />;
+}
---
+++ src/pages/privacy-policy.jsx
@@ -0,0 +1,5 @@
+import LegalPage from "../components/LegalPage";
+
+export default function PrivacyPolicy() {
+  return <LegalPage title="Privacy Policy" />;
+}
