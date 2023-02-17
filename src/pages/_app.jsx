e14d65a5ee956d3e35465f8a5c7855dd56abae72
prabk123 <prabodhkakodkar@gmail.com>
2023-02-17 18:06:42 +0000

    add crisp chat

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,12 @@
-import 'focus-visible'
-import '@/styles/tailwind.css'
+import "focus-visible";
+import "@/styles/tailwind.css";
+import CrispChat from "../components/CrispChat";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CrispChat />
+      <Component {...pageProps} />
+    </>
+  );
 }
