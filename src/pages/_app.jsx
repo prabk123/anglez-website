a940865ef0e63ee59cece14239c3249c60e72d91
prabk123 <prabodhkakodkar@gmail.com>
2023-05-01 12:33:46 +0100

    add analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,10 +2,26 @@ import "focus-visible";
 import "@/styles/tailwind.css";
 import CrispChat from "../components/CrispChat";
 import { initializeTapfiliate } from "../utils/tapfiliate";
+import analytics from "../utils/analytics";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 initializeTapfiliate();
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  function trackPage() {
+    analytics.page();
+  }
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", trackPage);
+    return () => {
+      router.events.off("routeChangeComplete", trackPage);
+    };
+  }, [router.events]);
+
   return (
     <>
       <CrispChat />
